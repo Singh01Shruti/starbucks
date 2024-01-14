893febe549f7c3f43cf8b4cd4b51ec85a50d8788
Shruti Singh <singh001shruti@gmail.com>
2024-01-14 16:04:38 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Header/Navbar"
 import Main from "./Components/Main/Main"
+import Section from "./Components/Section/Section"
 function App() {
 
   return (
     <> 
       <Navbar></Navbar>
       <Main></Main>
+      <Section></Section>
     </>
   )
 }
--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -1,8 +1,27 @@
-
+import './Section.css'
 export default function Section(){
     return (
         <>
-        <div></div>
+        <div>
+            <div className="section1">
+            <h1>Getting started is easy</h1>
+            <p>Earn Stars and get rewarded in a few easy steps.</p>
+            </div>
+            <div className="section2">
+            <div className="section3">
+                <h2>Create an account</h2>
+                <p>To get started, <a href="#" className="join-link">join now</a>. You can also <a href="#" className="join-link">join in the app</a> to get access to the full range of Starbucks® Rewards benefits.</p>
+            </div>
+            <div className="section3">
+             <h2>Order and pay how you'd like</h2>
+             <p>Use cash, credit/debit card or save some time and pay right through the app. You'll collect Stars all ways. <a href="#" className="join-link">Learn how</a></p>
+            </div>
+            <div className="section3">
+            <h2>Earn Stars, get Rewards</h2>
+            <p>As you earn Stars, you can redeem them for Rewards—like free food, drinks, and more. Start redeeming with as little as 25 Stars!</p>
+            </div>
+            </div>
+        </div>
         </>
     )
 }
